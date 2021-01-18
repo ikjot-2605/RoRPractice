536ea746158fb84c1b61e416db3a0e800f80d8e5
Ikjot Singh <ikjotsd.191cs123@nitk.edu.in>
2021-01-18 19:43:24 +0530

    blog-project-basic

---
+++ blog_project/app/models/ability.rb
@@ -0,0 +1,28 @@
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    # All users
+    can :index, Article
+
+    # Can read public articles
+    can :show, Article, public: true
+
+    # Additional permissions for logged in users
+    if user.present?
+      # Can read private articles
+      can :show, Article, public: false
+
+      # Can create articles
+      can :new, Article
+      can :create, Article
+
+      # Can edit their own articles
+      can :edit, Article, user_id: user.id
+      can :update, Article, user_id: user.id
+
+      # Can destroy their own articles
+      can :destroy, Article, user_id: user.id
+    end
+  end
+end
